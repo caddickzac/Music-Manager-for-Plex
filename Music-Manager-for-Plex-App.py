e98009f8dad765ed3008f29b402f8db9dd842a5c
Zac Caddick <caddickzac@gmail.com>
2026-03-07 11:20:26 -0500

    Updated Screenshots to include new modules

--- Music-Manager-for-Plex-App.py
+++ Music-Manager-for-Plex-App.py
@@ -23,7 +23,7 @@ except ImportError:
 from Scripts import plex_galaxy, artist_recommender  # Import from the subfolder
 
 # --- Version Configuration ---
-CURRENT_VERSION = "v2.1.5"
+CURRENT_VERSION = "v2.1.6"
 REPO_OWNER = "caddickzac"
 REPO_NAME = "Music-Manager-for-Plex"
 
